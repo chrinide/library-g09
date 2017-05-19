82d2fdf96614fa1e36ed1e2b416ac9500037e530
RANGSIMAN KETKAEW <rangsiman1993@gmail.com>
2017-05-19 22:47:18 +0700

    Update g09-extract-optimized-energy.sh

--- g09-extract-optimized-energy.sh
+++ g09-extract-optimized-energy.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 cd /home/$USER/
 mkdir -r extract_energy
 cd ./extract_energy
@@ -7,7 +5,7 @@ cd ./extract_energy
 for logfile in *.log
 do
  echo ${logfile}
- sed -n '/SCF D/{n;p;}' ${logfile}
+ sed -n '/SCF Done/{n;p;}' ${logfile}
  printf "--------------------------------\n" > ${logfile}_extracted_energy
  echo '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n' > ${logfile}_extracted_energy
 done
